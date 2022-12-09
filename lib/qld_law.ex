5626abcb93c9270304c3fb930e261913064777ca
striking <chris@strikinggroup.com.au>
2022-12-09 17:17:08 +1000

    split on public notices

--- lib/qld_law.ex
+++ lib/qld_law.ex
@@ -1,15 +1,16 @@
 defmodule QldLaw do
 
   def extract_public_notices(file \\ "priv/test.pdf") do
-
-    with  {:ok, content} <- PdfToText.from_path(file) do
-      content
-      |> parse_text  #pparse text
-    end
+    {:ok, content} = PdfToText.from_path(file)
+      # content
+      # |> parse_text  #pparse text
   end
+7
 
-  def parse_text(string) do
+  def parse_text(content) do
     # string
+    String.split(content, ~r/\n\n;PUBLIC NOTICES\nNotice of intention to apply for Grant\nof Probate or Letters of Administration\n/)
+    |> List.last
     # |> String.split() 
   end
 end
