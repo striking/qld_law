4a93381f60209d556cf34193fc9467d184e0a2a8
striking <chris@strikinggroup.com.au>
2022-12-10 12:48:03 +1000

    using named_cpatures to extract key information

--- lib/qld_law.ex
+++ lib/qld_law.ex
@@ -5,12 +5,47 @@ defmodule QldLaw do
       # content
       # |> parse_text  #pparse text
   end
-7
+
+@suburbs ~w[
+    Alderley Enoggera Gaythorne Grange Lutwyche Newmarket Herston Wilston Everton Mcdowall 
+    Mitchelton Chermside Grovely Keperra Dorrington Stafford Gordon Kedroin Kalinga Lytwyche 
+    Windsor Wooloowin Nundah Northgate Toombul Wavell Clayfield Albion Hendra Milton Paddington 
+    Rosalie Gap Ashgrove Bardon Rainworth Auchenflower Torwood Jubilee Toowong Chelmer 
+    Indooroopilly Taringa Sherwood Graceville Tennyson Oxley Corinda Rocklea Inala Darra 
+    Richlands Durack Doolandella Pallara 
+  ]
+
+  
 
   def parse_text(content) do
     # string
     String.split(content, ~r/\n\n;PUBLIC NOTICES\nNotice of intention to apply for Grant\nof Probate or Letters of Administration\n/)
     |> List.last
-    |> String.split(~r/\n?\n;?[A-Z]\w+,\s[A-Z]\w+([A-Z]\w)?/ ) 
+    |> String.split(~r/\n?\n;?[A-Z]\w+,\s[A-Z]\w+([A-Z]\w)?/)
+    |> filter_by_suburb
+  end
+
+  def filter_by_suburb(content) do
+    Enum.filter(content, &String.contains?(&1, @suburbs)) 
+  end
+
+  def extract_first_name(content) do
+    Regex.named_captures(~r/of\s(?<firstname>[A-Z]{3,}\w+)\n?\s?/, content)
+  end
+
+  def extract_last_name(content) do
+    Regex.named_captures(~r/(?<lastname>[A-Z]{3,}\w+)\n?\s?late/, content)
+  end
+
+  def extract_middle_name(content) do
+    Regex.named_captures(~r/[A-Z]{3,}\w+\n?\s?(?<middlename>[A-Z]{3,}\w+)\s[A-Z]\w+\slate/, content)
+  end
+
+  def extract_address(content) do
+    Regex.named_captures(~r/of\s(?<address>[0-9]{1,}\/?[0-9]{1,}\s[A-Za-z]\w+\s[A-Za-z]\w+)\,/, content)
+  end
+
+  def extract_suburb(content) do
+    Regex.named_captures(~r/\,\s(?<suburb>[A-Z][a-z]\w+(\s[A-Z][a-z]\w+)?)\s?\,?/, content)
   end
 end
